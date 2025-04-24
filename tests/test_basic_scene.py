20e33d75930098f89ec3551ef35189529178703b
Talley Lambert <talley.lambert@gmail.com>
2025-04-24 10:10:54 -0400

    add assertion for model_dump_json in test_basic_view

--- tests/test_basic_scene.py
+++ tests/test_basic_scene.py
@@ -4,3 +4,4 @@ import scenex as snx
 def test_basic_view(basic_view: snx.View) -> None:
     snx.show(basic_view)
     assert isinstance(basic_view.model_dump(), dict)
+    assert isinstance(basic_view.model_dump_json(), str)
