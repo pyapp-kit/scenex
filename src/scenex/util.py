7911c8f880999e2d45b1d249908b7362eb1650b7
Talley Lambert <talley.lambert@gmail.com>
2025-04-29 15:46:16 -0400

    refactor: update node initialization and improve show function documentation

--- src/scenex/util.py
+++ src/scenex/util.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
+import logging
 from collections.abc import Callable, Iterable, Iterator
 from contextlib import suppress
-from typing import TYPE_CHECKING, Any, Protocol, no_type_check
+from typing import TYPE_CHECKING, Any, Protocol
 
-from .model import Node, View
+from scenex import model
 
 if TYPE_CHECKING:
 
@@ -19,49 +20,79 @@ if TYPE_CHECKING:
 
 __all__ = ["show", "tree_repr"]
 
+logger = logging.getLogger("scenex")
 
-# FIXME: this is a temporary solution to deal with pyright and pydantic validation
-# not yet sure what the equivalent of pydantic.mypy is for pyright
-@no_type_check
-def show(obj: Node | View) -> None:
-    """Show the scene."""
-    from scenex import model
 
+def show(obj: model.Node | model.View | model.Canvas) -> None:
+    """Show a scene or view.
+
+    Parameters
+    ----------
+    obj : Node | View
+        The scene or view to show. If a Node is provided, it will be wrapped in a Scene
+        and then in a View.
+    """
     from .adaptors.auto import get_adaptor_registry
 
-    adaptors = get_adaptor_registry()
-    if isinstance(obj, View):
-        view = obj
-    elif isinstance(obj, model.Scene):
-        view = View(scene=obj)
-    elif isinstance(obj, Node):
-        scene = model.Scene(children=[obj])
-        view = View(scene=scene)
+    if isinstance(obj, model.Canvas):
+        canvas = obj
+    else:
+        if isinstance(obj, model.View):
+            view = obj
+        elif isinstance(obj, model.Scene):
+            view = model.View(scene=obj)
+        elif isinstance(obj, model.Node):
+            scene = model.Scene(children=[obj])
+            view = model.View(scene=scene)
 
-    canvas = model.Canvas(views=[view])
+        canvas = model.Canvas(views=[view])  # pyright: ignore[reportArgumentType]
 
-    adaptors.get_adaptor(canvas)
-    canvas.show()
-    cam = adaptors.get_adaptor(view.camera)
-    cam._snx_set_range(0.1)
+    canvas.visible = True
+    reg = get_adaptor_registry()
+    reg.get_adaptor(canvas, create=True)
+    for view in canvas.views:
+        cam = reg.get_adaptor(view.camera)
+        cam._snx_set_range(0.1)
+
+        native_scene = view.scene._get_native()
+        logger.debug("SHOW %s", tree_repr(native_scene))
 
 
 def tree_repr(
     node: SupportsChildren,
     *,
     node_repr: Callable[[Any], str] = object.__repr__,
-    prefix: str = "",
-    is_last: bool = True,
+    _prefix: str = "",
+    _is_last: bool = True,
 ) -> str:
     """
-    Return an ASCII/Unicode tree representation of self and its descendants.
+    Return an ASCII/Unicode tree representation of `node` and its descendants.
 
-    Assumes `node.children` is an iterable of objects that also implement
-    `tree_repr(prefix, is_last)` (or simply this same function via mixin/monkey-patch).
+    This assumes that `node` is a tree-like object with a `children` attribute that is
+    either a property or a callable that returns an iterable of child nodes.
+
+    Parameters
+    ----------
+    node : SupportsChildren
+        Any object that has a `children` attribute or method that returns an iterable
+        of child nodes.
+    node_repr : Callable[[Any], str], optional
+        Function to convert the node to a string. Defaults to `object.__repr__` (which
+        avoids complex repr functions on objects, but use `repr` if you want to see
+        the full representation).
+    prefix : str, optional
+        Prefix to use for each line of the tree. Defaults to an empty string.
+    is_last : bool, optional
+        Whether this node is the last child of its parent. Defaults to `True`.
+        This is used to determine the branch character to use in the tree
+        representation.
     """
-    branch = "└── " if is_last else "├── "
-    lines: list[str] = [f"{prefix}{branch}{node_repr(node)}"]
-    prefix_child = prefix + ("    " if is_last else "│   ")
+    if _prefix:
+        branch = "└── " if _is_last else "├── "
+    else:
+        branch = ""
+    lines: list[str] = [f"{_prefix}{branch}{node_repr(node)}"]
+    prefix_child = _prefix + ("    " if _is_last else "│   ")
 
     if (children := getattr(node, "children", None)) is None:
         return "\n".join(lines)
@@ -70,7 +101,10 @@ def tree_repr(
     for child, is_last_child in _iter_with_last_flag(children):
         lines.append(
             tree_repr(
-                child, node_repr=node_repr, prefix=prefix_child, is_last=is_last_child
+                child,
+                node_repr=node_repr,
+                _prefix=prefix_child,
+                _is_last=is_last_child,
             )
         )
     return "\n".join(lines)
