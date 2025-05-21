23eda16621d9f22e38d3e1d7258e4faf6145ec25
Gabriel Selzer <gjselzer@wisc.edu>
2025-05-21 11:21:27 -0500

    Misc fixes

--- src/scenex/adaptors/_auto.py
+++ src/scenex/adaptors/_auto.py
@@ -1,29 +1,41 @@
 import importlib.util
+import os
 
 from ._registry import AdaptorRegistry
 
 
 def get_adaptor_registry(backend: str | None = None) -> AdaptorRegistry:
     """Get the backend adaptor registry."""
-    adaptors: AdaptorRegistry
+    # Load backend explicitly requested by user
     if backend == "vispy":
-        from ._vispy import adaptors
-
-        return adaptors
+        return _load_vispy_adaptors()
     if backend == "pygfx":
-        from ._pygfx import adaptors
+        return _load_pygfx_adaptors()
 
-        return adaptors
+    # Load backend requested via environment variables
+    env_request = os.environ.get("SCENEX_CANVAS_BACKEND", None)
+    if env_request == "vispy":
+        return _load_vispy_adaptors()
+    if env_request == "pygfx":
+        return _load_pygfx_adaptors()
 
     # If no backend is specified, try to find one
     if importlib.util.find_spec("vispy") is not None:
-        from ._vispy import adaptors
-
-        return adaptors
+        return _load_vispy_adaptors()
     if importlib.util.find_spec("pygfx") is not None:
-        from ._pygfx import adaptors
-
-        return adaptors
+        return _load_pygfx_adaptors()
 
     for_backend = f" for backend {backend!r}" if backend else ""
     raise RuntimeError(f"No provider found{for_backend} :(")
+
+
+def _load_pygfx_adaptors() -> AdaptorRegistry:
+    from ._pygfx import adaptors
+
+    return adaptors
+
+
+def _load_vispy_adaptors() -> AdaptorRegistry:
+    from ._vispy import adaptors
+
+    return adaptors
