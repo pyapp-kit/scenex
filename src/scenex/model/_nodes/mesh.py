6dd4335ca41115def296d16a175c4e3d0ba89424
Gabriel Selzer <gjselzer@wisc.edu>
2025-10-02 17:25:47 -0500

    Add basic passes-through implementation
    I don't really understand it though :)

--- src/scenex/model/_nodes/mesh.py
+++ src/scenex/model/_nodes/mesh.py
@@ -44,5 +44,41 @@ class Mesh(Node):
         )  # type: ignore
 
     def passes_through(self, ray: Ray) -> float | None:
-        # TODO
-        return None
+        # Möller-Trumbore intersection algorithm, vectorized over all triangles
+        # Adapted from https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm#C++_implementation
+
+        # TODO: Better documentation. I don't yet understand the math
+        e1 = self.vertices[self.faces[:, 1]] - self.vertices[self.faces[:, 0]]
+        e2 = self.vertices[self.faces[:, 2]] - self.vertices[self.faces[:, 0]]
+        # Ignore triangles parallel to the ray
+        ray_cross_e2 = np.cross(ray.direction, e2)
+        # Vectorized version of row-wise dot product of ray_cross_e2 and e1
+        det = np.sum(ray_cross_e2 * e1, axis=1)
+        parallel_triangles = np.isclose(det, 0)
+
+        # Refactor variables to avoid parallel triangles
+        e1 = e1[~parallel_triangles]
+        e2 = e2[~parallel_triangles]
+        ray_cross_e2 = ray_cross_e2[~parallel_triangles]
+        det = det[~parallel_triangles]
+        v1 = self.vertices[self.faces[:, 0]][~parallel_triangles]
+
+        inv_det = 1 / det
+        s = ray.origin - v1
+        u = inv_det * np.sum(s * ray_cross_e2, axis=1)
+
+        s_cross_e1 = np.cross(s, e1)
+        v = inv_det * np.sum(ray.direction * s_cross_e1, axis=1)
+
+        intersecting = (u >= 0) & (v >= 0) & (u + v < 1)
+
+        if not np.any(intersecting):
+            return None
+        inv_det = inv_det[intersecting]
+        e2 = e2[intersecting]
+        s_cross_e1 = s_cross_e1[intersecting]
+
+        t = inv_det * np.sum(e2 * s_cross_e1, axis=1)
+        print(f"{ray.origin}: {t}")
+
+        return float(np.min(t))
---
+++ tests/model/_nodes/test_mesh.py
@@ -0,0 +1,51 @@
+import cmap
+import numpy as np
+import pytest
+
+import scenex as snx
+from scenex import Mesh
+from scenex.app.events import Ray
+
+
+@pytest.fixture
+def mesh() -> snx.Mesh:
+    vertices = np.array(
+        [
+            [0, 0, 0],  # 0
+            [1, 0, 0],  # 1
+            [0, 1, 0],  # 2
+        ]
+    )
+    faces = np.array(
+        [
+            [0, 1, 2],
+        ]
+    )
+    return snx.Mesh(vertices=vertices, faces=faces, color=cmap.Color("red"))
+
+
+def test_bounding_box(mesh: snx.Mesh) -> None:
+    exp_bounding_box = np.asarray(((0, 0, 0), (1, 1, 0)))
+    assert np.array_equal(exp_bounding_box, mesh.bounding_box)
+
+
+def test_passes_through(mesh: Mesh) -> None:
+    # Check a ray that passes through the mesh hits
+    ray = Ray(origin=(0.25, 0.25, 1), direction=(0, 0, -1))
+    assert mesh.passes_through(ray) == 1
+
+    # Check a ray that grazes the left edge of the image hits
+    ray = Ray(origin=(0, 0.5, 1), direction=(0, 0, -1))
+    assert mesh.passes_through(ray) == 1
+
+    # Check a ray that grazes the right edge of the image misses
+    ray = Ray(origin=(0.5, 0.5, 1), direction=(0, 0, -1))
+    assert mesh.passes_through(ray) is None
+
+    # Check a ray that does not pass through the image misses
+    ray = Ray(origin=(-50, -50, 1), direction=(0, 0, -1))
+    assert mesh.passes_through(ray) is None
+
+    # Check a ray that is perpendicular to the image misses
+    ray = Ray(origin=(0, 0, 0), direction=(-1, 0, 0))
+    assert mesh.passes_through(ray) is None
