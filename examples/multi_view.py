ce86907e02454c1400cdbc2fa82d26aef3d2bfba
Gabriel Selzer <gjselzer@wisc.edu>
2025-10-15 15:09:40 -0500

    Make the example work for VisPy
    Along with a FIXME to clean it up later

--- examples/multi_view.py
+++ examples/multi_view.py
@@ -17,39 +17,52 @@ try:
 except ImportError:
     data = np.random.randint(0, 2, (60, 2, 128, 128)).astype(np.uint16)
 
-# The volume will show the first channel
-vol_data = data[:, 0, :, :]
-vol = snx.Volume(
-    blending=BlendMode.ADDITIVE,
-    data=vol_data,
-    clims=(vol_data.min(), vol_data.max()),
-)
-
-# The image will show the second channel
+imgs: list[snx.Image] = []
 img_data = data[:, 1, :, :]
-img = snx.Image(
-    data=img_data[0],
-    blending=BlendMode.ADDITIVE,
-    cmap=cmap.Colormap("magenta"),
-    clims=(img_data.min(), img_data.max()),
-    opacity=0,
-)
+
+vols: list[snx.Volume] = []
+vol_data = data[:, 0, :, :]
 
 
-# The scene contains both the volume and the image
-scene = snx.Scene(children=[img, vol])
+# FIXME: VisPy can't currently use two Cameras in the same Scene.
+# Until we enable multiple adaptors tied to the same model, we need multiple scenes
+# here. This is an internal limitation of VisPy's ViewBox, see:
+# https://github.com/vispy/vispy/blob/7b6b11c9d050bf2cc6f77844252e737d2b060579/vispy/scene/widgets/viewbox.py#L71
+def _make_scene() -> snx.Scene:
+    # The volume will show the first channel
+    vol = snx.Volume(
+        blending=BlendMode.ADDITIVE,
+        data=vol_data,
+        clims=(vol_data.min(), vol_data.max()),
+    )
+    vols.append(vol)
+
+    # The image will show the second channel
+    img = snx.Image(
+        data=img_data[0],
+        blending=BlendMode.ADDITIVE,
+        cmap=cmap.Colormap("magenta"),
+        clims=(img_data.min(), img_data.max()),
+        opacity=0,
+    )
+    imgs.append(img)
+
+    # The scene contains both the volume and the image
+    return snx.Scene(children=[vol, img])
+
 
 # We'll make two views on the same scene
 view1 = snx.View(
-    scene=scene,
+    scene=_make_scene(),
     camera=snx.Camera(interactive=True),
 )
 view2 = snx.View(
-    scene=scene,
+    scene=_make_scene(),
     camera=snx.Camera(interactive=True),
 )
 
 # And put them on the same canvas
+# canvas = snx.Canvas(views=[view1])
 canvas = snx.Canvas(views=[view1, view2])
 
 
@@ -58,14 +71,16 @@ canvas = snx.Canvas(views=[view1, view2])
 def _view1_event_filter(event: Event) -> bool:
     if isinstance(event, MouseMoveEvent):
         for node, distance in event.world_ray.intersections(view1.scene):
-            if node is vol:
+            if node in vols:
                 intersection = event.world_ray.point_at_distance(distance)
                 idx = max(0, min(59, round(intersection[2])))
-                img.data = img_data[idx]
-                img.transform = Transform().translated((0, 0, idx))
-                img.opacity = 1
+                for img in imgs:
+                    img.data = img_data[idx]
+                    img.transform = Transform().translated((0, 0, idx))
+                    img.opacity = 1
                 return False
-    img.opacity = 0
+    for img in imgs:
+        img.opacity = 0
     return False
 
 
@@ -75,7 +90,7 @@ snx.use("pygfx")
 snx.show(canvas)
 
 # Orbit around the center of the volume
-orbit_center = np.mean(np.asarray(view1.scene.bounding_box), axis=0)
+orbit_center = np.mean(np.asarray(view2.scene.bounding_box), axis=0)
 
 # The first camera can orbit around the center of the volume
 view1.camera.transform = Transform().translated(orbit_center).translated((300, 0, 0))
