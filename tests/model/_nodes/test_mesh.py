0867e3cd66c39c700adb28ff749abaaf2553f19c
Gabriel Selzer <gjselzer@wisc.edu>
2025-10-17 10:20:57 -0500

    Pass source in mesh tests

--- tests/model/_nodes/test_mesh.py
+++ tests/model/_nodes/test_mesh.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock
+
 import cmap
 import numpy as np
 import pytest
@@ -31,21 +33,27 @@ def test_bounding_box(mesh: snx.Mesh) -> None:
 
 def test_passes_through(mesh: Mesh) -> None:
     # Check a ray that passes through the mesh hits
-    ray = Ray(origin=(0.25, 0.25, 1), direction=(0, 0, -1))
+    ray = Ray(
+        origin=(0.25, 0.25, 1), direction=(0, 0, -1), source=MagicMock(spec=snx.View)
+    )
     assert mesh.passes_through(ray) == 1
 
     # Check a ray that grazes the left edge of the image hits
-    ray = Ray(origin=(0, 0.5, 1), direction=(0, 0, -1))
+    ray = Ray(origin=(0, 0.5, 1), direction=(0, 0, -1), source=MagicMock(spec=snx.View))
     assert mesh.passes_through(ray) == 1
 
     # Check a ray that grazes the right edge of the image misses
-    ray = Ray(origin=(0.5, 0.5, 1), direction=(0, 0, -1))
+    ray = Ray(
+        origin=(0.5, 0.5, 1), direction=(0, 0, -1), source=MagicMock(spec=snx.View)
+    )
     assert mesh.passes_through(ray) is None
 
     # Check a ray that does not pass through the image misses
-    ray = Ray(origin=(-50, -50, 1), direction=(0, 0, -1))
+    ray = Ray(
+        origin=(-50, -50, 1), direction=(0, 0, -1), source=MagicMock(spec=snx.View)
+    )
     assert mesh.passes_through(ray) is None
 
     # Check a ray that is perpendicular to the image misses
-    ray = Ray(origin=(0, 0, 0), direction=(-1, 0, 0))
+    ray = Ray(origin=(0, 0, 0), direction=(-1, 0, 0), source=MagicMock(spec=snx.View))
     assert mesh.passes_through(ray) is None
