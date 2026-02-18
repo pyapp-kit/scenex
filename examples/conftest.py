6c4af39a7320405ddddca9f9627c8b67e32aeee8
Gabriel Selzer <gjselzer@wisc.edu>
2026-02-17 20:40:02 -0600

    Conditionally skip when not testing jupyter

---
+++ examples/conftest.py
@@ -0,0 +1,35 @@
+"""Pytest setup for notebook tests."""
+
+from typing import TYPE_CHECKING
+
+from nbval.plugin import IPyNbCell  # type: ignore
+
+if TYPE_CHECKING:
+    import pytest
+
+
+def pytest_collection_modifyitems(
+    session: "pytest.Session", config: "pytest.Config", items: "list[pytest.Item]"
+) -> None:
+    """Add sanitizers to Notebook tests."""
+    if IPyNbCell is None:
+        return  # nbval not installed, nothing to sanitize
+
+    for item in items:
+        # For each notebook cell being tested...
+        if not isinstance(item, IPyNbCell):
+            continue
+        # Add the following (regex) sanitations...
+        # (Note that both the expected and actual cell outputs will be sanitized)
+        item.parent.sanitize_patterns.update(  # pyright: ignore
+            {
+                # -- Convert all jupyter canvases to the same (pygfx) class name -- #
+                r"CanvasBackend": "JupyterRenderCanvas",
+                # -- Convert canvas sizes to integers (vispy uses floats) -- #
+                r"(\d+)\.0px": r"\1px",  # e.g. 600.0px -> 600px
+                # -- Normalize div snapshot ids (unique to each run) -- #
+                r"snapshot-[a-f0-9]+": "snapshot-XXXXX",
+                # -- Normalize canvas data (within the img tag) -- #
+                r"data:image/png;base64,[A-Za-z0-9+/=]+": "data:image/png;base64,XXXXX",
+            }
+        )
