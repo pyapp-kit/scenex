0cdff7fce5e2dca6b48666ea7d4d1596867daf5d
Gabriel Selzer <gjselzer@wisc.edu>
2026-02-20 17:00:40 -0600

    Ignore all stderr on pygfx
    Not sure we could do better with the current state of nbval

--- examples/conftest.py
+++ examples/conftest.py
@@ -16,6 +16,17 @@ def pytest_collection_modifyitems(
         # For each notebook cell being tested...
         if not isinstance(item, IPyNbCell):
             continue
+        try:
+            # FIXME: On pygfx some lines are printed to stderr that we don't want to
+            # compare. Ideally we could ignore ONLY these lines, e.g.
+            # libEGL warning: DRI3 error: Could not get DRI3 device
+            # libEGL warning: Ensure your X server supports DRI3 to get accelerated...
+            # Unable to find extension: VK_EXT_physical_device_drm
+            import pygfx  # noqa: F401
+
+            item.parent.skip_compare += ("stderr",)  # pyright: ignore
+        except ImportError:
+            pass
 
         # Add the following (regex) sanitations...
         # (Note that both the expected and actual cell outputs will be sanitized)
