6b62afae11ebd799a05028617a35dbf5a1c898f9
Gabriel Selzer <gjselzer@wisc.edu>
2026-05-18 13:17:14 -0500

    Clean up the tests a little bit

--- tests/app/test_auto.py
+++ tests/app/test_auto.py
@@ -2,15 +2,18 @@
 
 import pytest
 
-from scenex.app import app, determine_app
+from scenex.app import GuiFrontend, app, determine_app
 
 
 @pytest.mark.skipif(
-    determine_app() == "jupyter", reason="IPython has its own exception handling`."
+    determine_app() == GuiFrontend.JUPYTER,
+    reason="IPython has its own exception handling.",
 )
 def test_excepthook(capsys: pytest.CaptureFixture) -> None:
     """Tests that exceptions raised in the app's event loop don't crash the app."""
     qapp = app()
+    # NOTE this create_app call is needed to counter pytestqt's patch of sys.excepthook
+    qapp.create_app()
 
     def raise_exception() -> None:
         raise NotImplementedError("Test exception")
